5f4dc15494d015ce344d4bf2879d9b90b5b57f7a
Saurabh Jain <jsourabh861@gmail.com>
2021-09-16 20:52:47 +0530

    Update script.js

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -13,7 +13,7 @@ document.getElementById('submitButton').addEventListener('click', async function
         showError('Please enter a valid URL!');
     } else {
         const url = input.value;
-        const reqUrl = `https://git.heroku.com/minify-java.gitapi?url=${url}`;
+        const reqUrl = `https://git.heroku.com/minify-java.git/api?url=${url}`;
         if (isValid(url)) {
             spinner.classList.toggle('visually-hidden');
             const res = await fetch(reqUrl, {
